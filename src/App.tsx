f73a6dadeebca064c9b38f88ae9aebff544a5b5e
Nina_Tang <nina_tang@wistron.com>
2022-11-24 11:27:03 +0800

    Update for trigger action

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React update for page
         </a>
       </header>
     </div>
